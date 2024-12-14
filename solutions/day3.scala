4c5559d4dfcd0fd5f2b14e6deae943ec1a62ffc3
ChidiRNweke <chidi125@gmail.com>
2024-12-14 18:51:20 +0100

    Add exercise 2's solution

--- solutions/day3.scala
+++ solutions/day3.scala
@@ -9,8 +9,7 @@ import cats.syntax.all._
 import cats.parse.Parser
 import cats.parse.Rfc5234.digit
 
-@main def day3 =
-  val data = readFile("day3.txt").get
+def exercise1(data: String): Int =
   val mulParser = Parser.string("mul").void
   val leftBracketParser = Parser.char('(').void
   val rightBracketParser = Parser.char(')').void
@@ -24,11 +23,30 @@ import cats.parse.Rfc5234.digit
 
   val validParser =
     mulParser *> leftBracketParser *> digitParser <* rightBracketParser
-  val untilValidInput = Parser.anyChar.repUntil(validParser.backtrack).void
+  val invalidInput = Parser.anyChar.repUntil(validParser.backtrack).void
 
-  val multiplicationParser = validParser.surroundedBy(untilValidInput.?).rep
+  val multiplicationParser = validParser.surroundedBy(invalidInput.?).rep
 
-  val numbers = multiplicationParser
+  multiplicationParser
     .parseAll(data)
     .map(_.sumAll)
+    .getOrElse(throw new Exception("Parsing failed"))
+
+def filterDonts(data: String): String =
+  val dontParser = Parser.string("don't()").void
+  val doParser = Parser.string("do()").void
+  val discardDont =
+    (dontParser ~ Parser.anyChar.repUntil(doParser.backtrack)).void
+  Parser.anyChar
+    .surroundedBy(discardDont.?)
+    .repAs[String]
+    .parseAll(data)
+    .getOrElse(throw new Exception("Parsing failed"))
+
+@main def day3 =
+  val data = readFile("day3.txt").get
+  val numbers = exercise1(data)
   println(numbers)
+  val filtered = filterDonts(data)
+  val ex2 = exercise1(filtered)
+  println(ex2)
